71c9e339ed49d61c2c07d47148f2abe61056d034
Benjamin Fernandes <benjamin_fernandes10@hotmail.com>
2025-06-21 14:46:06 -0300

    value objects

--- src/@core/events/domain/entities/customer.entity.ts
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -1,26 +1,34 @@
 import { AggregateRoot } from "src/@core/common/domain/aggregate-root";
 import { Entity } from "src/@core/common/domain/entity";
+import Cpf from "src/@core/common/domain/value-objects/cpf.vo";
+import Uuid from "src/@core/common/domain/value-objects/uuid.vo";
+
+export class CustomerId extends Uuid {}
 
 export type CustomerConstructorProperties = {
-  id?: string;
-  cpf: string;
+  id?: CustomerId | string;
+  cpf: Cpf;
   name: string;
 }
 
 export class Customer extends AggregateRoot {
-  id?: string;
-  cpf: string;
+  id: CustomerId;
+  cpf: Cpf;
   name: string;
 
   constructor(props: CustomerConstructorProperties) {
     super();
-    this.id = props.id;
+    this.id = typeof props.id === 'string' ? new CustomerId(props.id) : props.id ?? new CustomerId();
     this.cpf = props.cpf;
     this.name = props.name;
   }
 
   static create(command: {name: string; cpf: string}) {
-    return new Customer(command);
+    const customer = new Customer({
+      name: command.name,
+      cpf: new Cpf(command.cpf),
+    });
+    return customer;
   }
 
   toJSON() {
